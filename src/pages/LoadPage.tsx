e425943bbc8433195dc9511121555129c3e40f87
毛质低下的狐狸 <38823575+xiririx@users.noreply.github.com>
2021-10-12 14:04:26 +0800

    Update LoadPage.tsx

--- src/pages/LoadPage.tsx
+++ src/pages/LoadPage.tsx
@@ -26,7 +26,7 @@ export const LoadPage: FC<LoadPageProps> = function ({ onLoad }) {
   const { updateParsedData, updatePage } = useGlobalContext();
   const params = qs.parse(document.location.search, {ignoreQueryPrefix: true})
   const uid = params.uid
-  const host = "https://erinilis.cn:7711"
+  const host = "https://114.219.35.114:9222"
   // const host = "http://127.0.0.1:7701"
   if (uid) {
     fetch(`${host}/genshin/gachalog/xlsx/${uid}`)
